7a8b70d7af2db9fcb457691d1d02bde3fe76d91d
Vitor <vitor@canaltech.com.br>
2021-07-08 00:20:16 -0300

    Integração com API do Prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,30 @@
-import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export function Header(){
-    return (
-        <header className={styles.headerContainer} >
-            <div className={styles.headerContent} >
-                <img src="/images/logo.svg" alt="" />
-                <nav>
-                    <a className={styles.active} >Home</a>
-                    <a>Posts</a>
-                </nav>
-                <SignInButton/>
-            </div>
-        </header>
-    )
+import { ActiveLink } from "../ActiveLink";
+
+export function Header() {
+  const { asPath } = useRouter();
+
+  console.log(asPath);
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="" />
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,75 @@
+import styles from "./styles.module.scss";
+
+import Head from "next/head";
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts | ig.news</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    Prismic.Predicates.at("document.type", "posts"),
+    {
+      fetch: ["posts.title", "posts.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
